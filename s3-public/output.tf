c8a0a3115bbca4f626ca88cf542b129eec93cb19
Heiya <morooka.akira.inon29@gmail.com>
2020-04-24 15:37:15 +0900

    Add endpoint output for s3.

--- s3-public/output.tf
+++ s3-public/output.tf
@@ -1,3 +1,7 @@
 output "bucket_name" {
   value = replace(aws_s3_bucket.public.bucket_domain_name, ".s3.amazonaws.com", "")
 }
+
+output "endpoint" {
+  value = aws_s3_bucket.public.website_endpoint
+}
