a5473b8439235f983bfb055381324c599d60ffda
Heiya <morooka.akira.inon29@gmail.com>
2020-06-08 10:06:41 +0900

    Fix: add output alb & target_group for ecs.

--- ecs/output.tf
+++ ecs/output.tf
@@ -5,3 +5,11 @@ output "cluster_name" {
 output "service_name" {
   value = aws_ecs_service.default.name
 }
+
+output "alb_id" {
+  value = aws_alb.default.id
+}
+
+output "alb_target_group_id" {
+  value = aws_alb_target_group.default.id
+}
